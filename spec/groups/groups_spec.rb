1bde955a820b89632f5467358168e532013107f4
Andrew Klotz <agc.klotz@gmail.com>
2020-09-06 20:34:44 +0000

    add a few more specs

---
+++ spec/groups/groups_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+describe "groups specs" do
+  it "should succeed" do
+    expect(true).to be(true)
+  end
+end
---
+++ spec/teams/teams_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+describe "teams specs" do
+  it "should succeed" do
+    expect(true).to be(true)
+  end
+end
