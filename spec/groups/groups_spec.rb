55970b313fc429a6d97e909d533ee6bd0a3c71f6
Andrew Klotz <agc.klotz@gmail.com>
2020-09-07 15:44:08 +0000

    too few tests to node balance

--- spec/groups/groups_spec.rb
+++ spec/groups/groups_spec.rb
@@ -4,4 +4,8 @@ describe "groups specs" do
   it "should succeed" do
     expect(true).to be(true)
   end
+
+  it "should still succeed" do
+    expect(true).to be(true)
+  end
 end
--- spec/teams/teams_spec.rb
+++ spec/teams/teams_spec.rb
@@ -4,4 +4,8 @@ describe "teams specs" do
   it "should succeed" do
     expect(true).to be(true)
   end
+
+  it "should still succeed" do
+    expect(true).to be(true)
+  end
 end
