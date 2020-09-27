42c316c5b8c2bad3cf0067d7e757c76b31f4004f
Andrew Klotz <agc.klotz@gmail.com>
2020-09-27 20:13:13 +0000

    add more specs
    Signed-off-by: Andrew Klotz <agc.klotz@gmail.com>

---
+++ spec/handlers/handlers_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe "handlers specs" do
+  it "should succeed" do
+    expect(true).to be(true)
+  end
+
+  it "should still succeed" do
+    expect(true).to be(true)
+  end
+end
---
+++ spec/workers/workers_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+describe "workers specs" do
+  it "should succeed" do
+    expect(true).to be(true)
+  end
+
+  it "should still succeed" do
+    expect(true).to be(true)
+  end
+end
+
+describe "workers second specs" do
+  it "should succeed" do
+    expect(true).to be(true)
+  end
+
+  it "should still succeed" do
+    expect(true).to be(true)
+  end
+end
